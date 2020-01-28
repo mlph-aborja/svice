ef17c01709d36748b446ebc5282385cb623f4368
Alfred Borja <alfredoborja81194@gmail.com>
2020-01-28 13:57:18 +0800

    Fix cors policy

--- svice-frontend/svice/src/app/signup/signup.service.js
+++ svice-frontend/svice/src/app/signup/signup.service.js
@@ -3,6 +3,10 @@ export function Register (params) : Promise<Response> {
   const url = process.env.REACT_APP_API_ENDPOINT + "/register";
   return fetch(url, {
     method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify(params)
   }).then(function(response) {
     return response.json();
