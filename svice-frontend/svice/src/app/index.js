ff4052d5979d484aa6524c538dc634f5eceb4a7d
Alfred Borja <alfredoborja81194@gmail.com>
2020-02-03 13:20:26 +0800

    create pages folder

--- svice-frontend/svice/src/app/index.js
+++ svice-frontend/svice/src/app/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 // styles for this kit
-import "../assets/css/bootstrap.min.css";
-import "../assets/scss/now-ui-kit.scss";
-import "../assets/demo/demo.css";
-import "../assets/demo/nucleo-icons-page-styles.css";
+import '../assets/css/bootstrap.min.css';
+import '../assets/scss/now-ui-kit.scss';
+import '../assets/demo/demo.css';
+import '../assets/demo/nucleo-icons-page-styles.css';
 
-import { SignUpPage } from "./signup";
-import { LoginPage } from "./login";
+import { SignUpPage } from './pages/signup';
+import { LoginPage } from './pages/login';
 
-export default function App (props) {
-	return (
-	  <BrowserRouter>
-	    <Switch>
-	      <Switch>
-			<Route path="/signup" render={props => <SignUpPage {...props} />} />
-			<Route path="/login" render={props => <LoginPage {...props} />} />
-	        <Redirect from="/" to="/signup" />
-	      </Switch>
-	    </Switch>
-	  </BrowserRouter>
-	);
+export default function App(props) {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Switch>
+          <Route path="/signup" render={props => <SignUpPage {...props} />} />
+          <Route path="/login" render={props => <LoginPage {...props} />} />
+          <Redirect from="/" to="/signup" />
+        </Switch>
+      </Switch>
+    </BrowserRouter>
+  );
 }
