eddc8a21f5e2db11580678e3827fb13b85e97b36
Eunice Orozco <eorozco@ideyatech.com>
2020-02-12 18:39:24 +0800

    dockerize

--- svice-backend/svice/run.sh
+++ svice-backend/svice/run.sh
@@ -4,7 +4,9 @@ cd /app
 php artisan cache:clear
 php artisan config:clear
 
-php artisan migrate:fresh --seed
+php artisan migrate:refresh --seed
+php artisan db:seed
 php artisan passport:install --force
 php artisan key:generate
+
 php artisan serve
